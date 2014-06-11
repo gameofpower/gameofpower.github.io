b88a47cf7a8f41a6f7b0e5f92b892b3f9f89fddb
Kamol Treewatchararat <Kamol.Treewatchararat@telescope.tv>
2014-06-11 14:44:16 -0700

    init commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global GameofpowerGithubIo, $*/
+
+
+window.GameofpowerGithubIo = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    GameofpowerGithubIo.init();
+});
